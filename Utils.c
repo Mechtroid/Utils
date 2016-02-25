a0ca005315b38ba491b2f5c52fbe2b951464f24c
Cole Wardell <cole.wardell@taloswest.com>
2016-02-25 10:42:53 -0800

    Updates to utils

--- Utils.c
+++ Utils.c
@@ -6,10 +6,10 @@
  */
 #include "Utils.h"
 #include <ctype.h>
+#include <math.h>
+#include "AString.h"
+#include <stdio.h>
 
-#ifndef _USE_STRFUNC
-#define _USE_STRFUNC 1
-#endif
 
 
 //================
@@ -39,7 +39,7 @@ unsigned char* fss(unsigned char* src, unsigned char split)
 		src[splitLoc] = '\0';
 		return (src+splitLoc+1);
 	}
-	else
+	else	//didn't find it, return pointer to end of string.
 	{
 		return (src+splitLoc);
 	}
@@ -67,12 +67,13 @@ unsigned char* fsp(unsigned char** src, unsigned char split)
  * NOTE: Might fail if leftSide has been modified.
  * WARNING: Passing in bad pointers has the possibility of writing out of bounds to bad memory.
 \*/
-void fssUndo(unsigned char* leftSide, unsigned char* rightSide, unsigned char split)
+void fsUndo(unsigned char* leftSide, unsigned char* rightSide, unsigned char split)
 {
 	if(rightSide > leftSide && rightSide[0] != '\0')
 	{
 		if(rightSide-1)
 		*(rightSide-1) = split;
+
 	}
 	else				//split char wasn't found
 	{
@@ -93,7 +94,10 @@ void strnCopy(uint8_t *dst, size_t dstSize, const unsigned char *src)
     *dst++ = *src++;
     dstSize--;
   }
-  *dst = '\0';
+  if(dstSize > 0)
+  {
+	  *dst = '\0';
+  }
 }
 
 /* Splits the string src into two halves at the first instance of the sentinel string/char.
@@ -125,12 +129,54 @@ unsigned int strSplit(unsigned char* left, size_t leftSize, unsigned char* right
 	return uOffset;
 }
 
+/*\
+ * STRing (Zero terminated) MATCH: Returns the location of a substring in a larger string, if found.
+ * If the substring is not found, return -1.
+ * NOTE: crudely case insensitive.
+\*/
+int strzMatch(unsigned char* string, unsigned char* substring)
+{
+	unsigned char *startPos = string, *currentPos, *currentSub;
+	while(*startPos != '\0')
+	{
+		currentPos = startPos;
+		currentSub = substring;
+		while(*currentPos != '\0' && *currentSub != '\0')
+		{
+			int diff = ((int)toupper(*currentPos) - (int)toupper(*currentSub));
+			if(diff == 0 )  //gotta be some sweet hexadecimal magic to make diff = 0 when it's 0, -32, or 32.
+			{
+				currentSub++;	//Match found, continue
+				currentPos++;
+			}
+			else
+			{
+				int i = 5;
+				break;
+			}
+		}
+		if(*currentSub == '\0')
+		{
+			return startPos - string;
+		}
+		else
+		{
+			startPos++;
+		}
+	}
+	return -1;
+}
+
 /* modify lNum to equal the base 10 number encoded in str, if possible.
- * If decoding fails, returns default.
+ * If decoding fails, returns 0. Otherwise returns 1.
  * returns lNum.
  */
-long int sToNum(long int* lNum, unsigned char* str)
+int sToNum(long int* lNum, unsigned char* str)
 {
+	if(*str == '\0' || isspace(*str))
+	{
+		return FALSE;
+	}
 	unsigned char** spTemp = &str;
 	long int lTemp = strtol(str, (char**)spTemp, 0);
 	if(**spTemp == '\0' || isspace(**spTemp))
@@ -138,65 +184,79 @@ long int sToNum(long int* lNum, unsigned char* str)
 		if(lNum != NULL)
 		{
 			*lNum = lTemp;
+			return TRUE;
 		}
 	}
-	return (lNum != NULL)? *lNum: lTemp;
+	return FALSE;
 
 }
 
+int sToFloat(float* pFloat, unsigned char* str)
+{
+	if(*str == '\0' || isspace(*str) || pFloat == NULL)
+	{
+		return FALSE;
+	}
+	unsigned char** spTemp = &str;
+	float dTemp = strtof(str, (char**)spTemp);
+	if(**spTemp == '\0' || isspace(**spTemp))
+	{
+		*pFloat = dTemp;
+		return TRUE;
+	}
+	return FALSE;
+}
 
+int sToDouble(double* pFloat, unsigned char* str)
+{
+	if(*str == '\0' || isspace(*str) || pFloat == NULL)
+	{
+		return FALSE;
+	}
+	unsigned char** spTemp = &str;
+	double dTemp = strtod(str, (char**)spTemp);
+	if(**spTemp == '\0' || isspace(**spTemp))
+	{
+		*pFloat = dTemp;
+		return TRUE;
+	}
+	return FALSE;
+}
+
+
+//Typical usage: AS_printf("%c%02u.%03u", floatMinus, floatOnes, floatMillis);
+void decomposeFloat(double dFloat, unsigned char* floatMinus, uint16_t* floatOnes, uint16_t* floatMillis)
+{
+	*floatMinus = ' ';
+	if(dFloat < 0)
+	{
+		*floatMinus = '-';
+		dFloat = -dFloat;
+	}
+	*floatOnes = (uint16_t) dFloat;
+	*floatMillis = (uint16_t) ((dFloat - *floatOnes) * 1000);
+
+}
 
 typedef struct //simple char buffer for the cprintf function.
 {
 	unsigned char (*sendChar)(unsigned char);
 	int idx, nchr;
 	uint8_t buf[64];
-} putbuff;
+} pbuff;
 
-static void putc_bfd 	(  				//Helper function for csprintf, below.
-						putbuff* pb,
+static void putc_bfrd 	(  				//Helper function for csprintf, below.
+						pbuff* pb,
 						char c
 						)
 {
 	unsigned int bw;
 	int i;
 
-
-	if (_USE_STRFUNC == 2 && c == '\n')	 /* LF -> CRLF conversion */
-		putc_bfd(pb, '\r');
-
 	i = pb->idx;	/* Buffer write index (-1:error) */
 	if (i < 0) return;
-
-#if _USE_LFN && _LFN_UNICODE
-#if _STRF_ENCODE == 3			/* Write a character in UTF-8 */
-	if (c < 0x80) {				/* 7-bit */
-		pb->buf[i++] = (uint8_t)c;
-	} else {
-		if (c < 0x800) {		/* 11-bit */
-			pb->buf[i++] = (uint8_t)(0xC0 | c >> 6);
-		} else {				/* 16-bit */
-			pb->buf[i++] = (uint8_t)(0xE0 | c >> 12);
-			pb->buf[i++] = (uint8_t)(0x80 | (c >> 6 & 0x3F));
-		}
-		pb->buf[i++] = (uint8_t)(0x80 | (c & 0x3F));
-	}
-#elif _STRF_ENCODE == 2			/* Write a character in UTF-16BE */
-	pb->buf[i++] = (uint8_t)(c >> 8);
+						/* Write a character without conversion */
 	pb->buf[i++] = (uint8_t)c;
-#elif _STRF_ENCODE == 1			/* Write a character in UTF-16LE */
-	pb->buf[i++] = (uint8_t)c;
-	pb->buf[i++] = (uint8_t)(c >> 8);
-#else							/* Write a character in ANSI/OEM */
-	c = ff_convert(c, 0);	/* Unicode -> OEM */
-	if (!c) c = '?';
-	if (c >= 0x100)
-		pb->buf[i++] = (uint8_t)(c >> 8);
-	pb->buf[i++] = (uint8_t)c;
-#endif
-#else							/* Write a character without conversion */
-	pb->buf[i++] = (uint8_t)c;
-#endif
 
 	if (i >= (int)(sizeof pb->buf) - 3) {	/* Send buffered characters */
 		bw = 0;
@@ -209,7 +269,31 @@ static void putc_bfd 	(  				//Helper function for csprintf, below.
 
 /* CPRINTF: _C_haracter _S_treaming implementation of _PRINTF_
  * This takes a string with character formatting and streams it character by character using the function given.
- * Shamelessly modified from FATfs's printf function.*/
+ * Accepted format tokens: %k,c,s;b,o,d,u,x,X;f; !!!Note: ALWAYS CAST FLOATING POINT ARGUMENTS TO DOUBLE!!!
+ * Token format: '%' ['-'|'0'] [<len>['.'<flen>]] ['l'|'L'] <type>
+ * Shamelessly modified from FATfs's printf function.
+ *
+ * Format string types:
+ * %k/K: skip  (for dynamically generated format strings using fixed argument lists)
+ * %c/C: single character
+ * %s/S: null-terminated string
+ * %b/B: binary
+ * %o/O: octal
+ * %d/D: integer
+ * %u/U: unsigned integer
+ * %x:	 hexidecimal (lowercase)
+ * %X:   Hexidecimal (Uppercase)
+ * %f/F: Fractional precision, soft. (Will truncate trailing 0s after rounding).   (ENSURE ARGUMENT FOR THIS TOKEN IS CAST TO A DOUBLE, NOT A FLOAT)
+ *
+ * Token prefix format: '%' ['-'|'0'] [<len>[.<flen>]] ['l'/'L'] <token>
+ * - 		Left justify
+ * 0		Pad right with zeroes
+ * <len>    Minimum Length/integer precision (strings/numbers)
+ * .<flen>	Override default fractional length (0 = maximum precision, otherwise <flen>, default 5)
+ * l/L		Read long (unsigned)int or full precision fractional number.  (WHICH IS A DOUBLE, NOT A FLOAT)
+ *
+ * !!!ALL ARGUMENTS FOR FRACTIONAL TOKENS WILL PRINT INCORRECTLY IF NOT CAST TO A DOUBLE!!!
+ */
 int csprintf(
 			unsigned char (*sendChar)(unsigned char),	/*pointer to a function that takes a character and either prints it, sends it, or stores it.*/
 			const unsigned char* fmt,					/*Formatted string to send*/
@@ -229,34 +313,46 @@ int csprintf_va	(											/*returns number of characters written.*/
 				)
 {
 	uint8_t f, r;
-	unsigned int nw, i, j, w;
+	unsigned  int n, nw, i, j, k, l, w, fw;
 	uint32_t v;
-	char c, d, s[16], *p;
-	putbuff pb;
+	char c, d, s[19], sf[17], *p;
+	size_t countof_s = sizeof s / sizeof s[0];
+	size_t countof_sf = sizeof sf / sizeof s[0];
+	double a, b, u;
+	pbuff pb;
 
 	pb.sendChar = sendChar;
 	pb.nchr = pb.idx = 0; /* Initialize output buffer */
 
 	for (;;) {
 		c = *fmt++;
-		if (c == 0) break;			/* End of string */
+		if (c == '\0') break;			/* End of string */
 		if (c != '%') {				/* Non escape character */
-			putc_bfd(&pb, c);
+			putc_bfrd(&pb, c);
 			continue;
 		}
-		w = f = 0;
+		f = 0; //Format flags
+		w = 0; //integer precision
+		fw = DEFAULT_FRACTIONAL_PRECISION;
 		c = *fmt++;
 		if (c == '0') {				/* Flag: '0' padding */
 			f = 1; c = *fmt++;
-		} else {
-			if (c == '-') {			/* Flag: left justified */
-				f = 2; c = *fmt++;
-			}
+		} else if (c == '-') {			/* Flag: left justified */
+		  f = 2; c = *fmt++;
 		}
-		while (IsDigit(c)) {		/* Precision */
+		while (IsDigit(c)) {		/* Integer Precision */
 			w = w * 10 + c - '0';
 			c = *fmt++;
 		}
+		if (c == '.') {				/* Floating point fractional digits precision */
+			c = *fmt++;
+			fw = 0;	//default to full precision.
+			while (IsDigit(c)) {
+				fw = fw * 10 + c - '0';
+				c = *fmt++;
+			}
+		}
+
 		if (c == 'l' || c == 'L') {	/* Prefix: Size is long int */
 			f |= 4; c = *fmt++;
 		}
@@ -264,17 +360,19 @@ int csprintf_va	(											/*returns number of characters written.*/
 		d = c;
 		if (IsLower(d)) d -= 0x20;
 		switch (d) {				/* Type is... */
+		case 'K' :					/* Skipped */
+			continue;
 		case 'S' :					/* String */
 			p = va_arg(arp, char*);
 			for (j = 0; p[j]; j++) ;
 			if (!(f & 2)) {
-				while (j++ < w) putc_bfd(&pb, ' ');
+				while (j++ < w) putc_bfrd(&pb, ' ');
 			}
-			while (*p) putc_bfd(&pb, *p++);
-			while (j++ < w) putc_bfd(&pb, ' ');
+			while (*p) putc_bfrd(&pb, *p++);
+			while (j++ < w) putc_bfrd(&pb, ' ');
 			continue;
 		case 'C' :					/* Character */
-			putc_bfd(&pb, (char)va_arg(arp, int)); continue;
+			putc_bfrd(&pb, (char)va_arg(arp, int)); continue;
 		case 'B' :					/* Binary */
 			r = 2; break;
 		case 'O' :					/* Octal */
@@ -284,8 +382,79 @@ int csprintf_va	(											/*returns number of characters written.*/
 			r = 10; break;
 		case 'X' :					/* Hexdecimal */
 			r = 16; break;
+		case 'F' :					/* Floating Point */
+			r = 10;					/* (always base 10 (but could theoretically handle other bases))*/
+			b = (double)va_arg(arp, double);
+
+			if(b < 0) {
+				b = -b;
+				f |= 8;
+			}
+
+			u = modf(b,&a);
+			i = k = l = 0;
+			j = (f & 4);
+
+			if(u > 0) {
+				fw = (fw && fw < countof_sf) ? fw : countof_sf;		/* Make fw maximum precision if undefined*/
+				do {												/* Fill sf with number after decimal place*/
+					u = u * r;										/* Shift decimal point right*/
+					d = (char)u; u = u - d;							/* Peel away digit to the left of decimal point*/
+					sf[i++] = d;									/* Store*/
+					l = d ? 0 : l + 1;								/* Keep running total of consecutive 0s*/
+					k = (d == r-1) ? k + 1 : 0; 					/* Keep running total of consecutive 9s*/
+				} while (u > DBL_EPSILON && i < fw && (j || (k < ROUND_MAGNITUDE && l < ROUND_MAGNITUDE)));
+				nw = i;
+				k = (2*u > 1) ? 1 : 0;				/* Use k as carry flag to round up*/
+				while(i--) {
+					if (k) {
+						if (sf[i] == r-1) {
+							sf[i] = 0xFF;			/* Turn consecutive 9s into trailing zeros*/
+						} else {
+							sf[i]++;
+							k = 0;
+						}
+					} else if (l) {
+						if(sf[i]== 0) {
+							sf[i] = 0xFF;			/* Turn consecutive 0s into trailing zeroes*/
+						}
+						else l = 0;
+					}
+				}
+
+				i = 0;								/* Reset i for future use*/
+				if(sf[0] != 0xFF)					/* Add decimal place as long as sf is not entirely trailing zeroes*/
+					s[i++] = '.';
+			}
+			else {
+				fw = nw = 0;
+			}
+
+			u = b + k;
+			do {									/* Generate integer part directly as string, backwards*/
+				d = (char)(fmod(u,r)); u /= r;
+				if (d > 9) d += 7 + 0x20 * IsLower(c);	/* Dumb bit logic to convert from number to ASCII*/
+				s[i++] = d + '0';
+			} while (u >= 1 && i < countof_s);
+			if (f & 8) s[i++] = '-';
+			j = i; d = (f & 1) ? '0' : ' ';
+			while (!(f & 2) && j++ < w+1) putc_bfrd(&pb, d);	/* Left pad*/
+			do putc_bfrd(&pb, s[--i]); while (i); i = 0;		/* Send integer part*/
+			while (i < nw) {									/* Send fractional part*/
+				k = sf[i++];
+				if (k == 0xFF && (f & 1)) k = 0;
+				if (k != 0xFF) {
+					if (k > 9) k += 7 + 0x20 * IsLower(c);
+					j++;
+					putc_bfrd(&pb,k + '0');
+				}
+			}
+			w+=fw;
+			while (j++ < w) putc_bfrd(&pb, d);					/* Right pad*/
+
+			continue;
 		default:					/* Unknown type (pass-through) */
-			putc_bfd(&pb, c); continue;
+			putc_bfrd(&pb, c); continue;
 		}
 
 		/* Get an argument and put it in numeral */
@@ -299,12 +468,12 @@ int csprintf_va	(											/*returns number of characters written.*/
 			d = (char)(v % r); v /= r;
 			if (d > 9) d += (c == 'x') ? 0x27 : 0x07;
 			s[i++] = d + '0';
-		} while (v && i < sizeof s / sizeof s[0]);
+		} while (v && i < countof_s);
 		if (f & 8) s[i++] = '-';
 		j = i; d = (f & 1) ? '0' : ' ';
-		while (!(f & 2) && j++ < w) putc_bfd(&pb, d);
-		do putc_bfd(&pb, s[--i]); while (i);
-		while (j++ < w) putc_bfd(&pb, d);
+		while (!(f & 2) && j++ < w) putc_bfrd(&pb, d);
+		do putc_bfrd(&pb, s[--i]); while (i);
+		while (j++ < w) putc_bfrd(&pb, d);
 	}
 	if(pb.idx >= 0)	/* Flush buffered characters to the file */
 	{
@@ -314,23 +483,21 @@ int csprintf_va	(											/*returns number of characters written.*/
 	}
 }
 
-
 //================
 //====FILE SYS====
 //================
-//(This function requires fatfs
 
 /* Lists all files in a directory and its subdirectories.
  * Example from http://elm-chan.org/fsw/ff/en/readdir.html
  */
-FRESULT scan_files (char* path)
+FRESULT scan_files (char* path, bool hidSys)
 /*path: Start node to be scanned (also used as work area) */
 {
 	FRESULT res;
 	FILINFO fno;
 	DIR dir;
 	int i;
-	char *fn;   /* This function assumes non-Unicode configuration */
+	unsigned char *fn;   /* This function assumes non-Unicode configuration */
 #if _USE_LFN
 	static char lfn[_MAX_LFN + 1];   /* Buffer to store the LFN */
 	fno.lfname = lfn;
@@ -346,19 +513,25 @@ FRESULT scan_files (char* path)
 		{
 			res = f_readdir(&dir, &fno);                   /* Read a directory item */
 			if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
-			if (fno.fname[0] == '.') continue;             /* Ignore dot entry */
+
+			if (fn[0] == '.') continue;                    /* Ignore dot entry */
 #if _USE_LFN
 			fn = *fno.lfname ? fno.lfname : fno.fname;
 #else
 			fn = fno.fname;
 #endif
+
+
+
+
 			if (fno.fattrib & AM_DIR) {                    /* It is a directory */
 				sprintf(&path[i], "/%s", fn);
-				res = scan_files(path);
+				res = scan_files(path, hidSys);
 				path[i] = 0;
 				if (res != FR_OK) break;
 			} else {                                       /* It is a file. */
-				AS_printf("%s/%s\n",path, fn);
+				if(hidSys && (strzMatch(fn,".") == -1)) continue;       /* Skip extensionless files if requested*/
+				AS_printf("%s/%s\r\n",path, fn);
 			}
 		}
 		f_closedir(&dir);
@@ -366,4 +539,3 @@ FRESULT scan_files (char* path)
 
 	return res;
 }
-
--- Utils.h
+++ Utils.h
@@ -11,17 +11,27 @@
 #include <stdlib.h>
 #include <STDarg.h>
 #include "stddef.h"
-
+#include "UTIL1.h"
+#include "ff.h"
 
 #define IsUpper(c)	(((c)>='A')&&((c)<='Z'))
 #define IsLower(c)	(((c)>='a')&&((c)<='z'))
 #define IsDigit(c)	(((c)>='0')&&((c)<='9'))
+#define IntAbs(c)   ((c<0) ? -c : c)
+#define DBL_EPSILON (2.220446e-16)
+#define DEFAULT_FRACTIONAL_PRECISION (5)
+#define ROUND_MAGNITUDE (2)
 
 unsigned char* fss(unsigned char* src, unsigned char split);
 void strnCopy(uint8_t *dst, size_t dstSize, const unsigned char *src);
 unsigned int strSplit(unsigned char*, size_t, unsigned char*, size_t, unsigned char*, unsigned char* );
-long int sToNum(long int*, unsigned char*);
+int sToNum(long int*, unsigned char*);
+int sToFloat(float*, unsigned char*);
+int sToDouble(double*, unsigned char*);
+void decomposeFloat(double , unsigned char* , uint16_t* , uint16_t* );
+int strzMatch(unsigned char* string, unsigned char* substring);
 int csprintf(unsigned char (*)(unsigned char), const unsigned char*, ...	);
 int csprintf_va(unsigned char (*)(unsigned char), const unsigned char*, va_list	);
-FRESULT scan_files(char*);
+
+FRESULT scan_files(char*, bool);
 #endif /* UTILS_H_ */
